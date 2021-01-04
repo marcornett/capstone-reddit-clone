b905fa5e8f7d5f9173b3fc04beaa71f40fbd02ac
Marcel Cornett <marcornett@gmail.com>
2021-01-04 15:06:14 -0500

    Hidding secret key.

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f%^9n#9fb@_ran4+p$=$488xsv_n$g#1!@vq)3y%ig_rxy)3gm'
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
