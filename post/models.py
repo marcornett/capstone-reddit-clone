0015db96676a877fb7bdc09edbf35e7d37bce831
Marcel Cornett <marcornett@gmail.com>
2021-01-05 09:57:49 -0500

    Post model inheriting from djanog model. Fields include: user, post, link, title, created at, updated at, up vote, down vote, and subreddit.

--- post/models.py
+++ post/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from user import User
+from subreddit import Subreddit
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    post = models.Charfield(
+        max_length=500,
+        blank=False,
+        null=True
+    )
+    link = models.URLField(
+        max_length=200,
+        blank=False,
+        null=True
+    )
+    title = models.CharField(
+        max_length=50,
+        blank=False,
+        null=True
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        auto_now_add=False
+    )
+    updated_at = models.DateTimeField(
+        default=timezone.now,
+        auto_now_add=False
+    )
+    up_vote = models.IntegerField(default=0)
+    down_vote = models.IntegerField(default=0)
+    subreddit = models.ForeignKey(
+        Subreddit, on_delete=models.CASCADE
+    )
