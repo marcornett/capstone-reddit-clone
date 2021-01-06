dcc77eca8081013f6bf725cee4b2e8985b4b3fdf
Cory Schlaudecker <cory.schlaudecker@gmail.com>
2021-01-05 19:40:25 -0500

    user model started added bio,likes,dislikes,images,subreddit

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from subreddit import Subreddit
 
-# Create your models here.
+class RedditUser(AbstractUser):
+    bio = models.TextField(max_length=250)
+    likes = models.IntergerField(default=0)
+    dislikes = models.IntergerField(default=0)
+    images = models.ImageField(upload_to=None)
+    subreddit = models.ManyToManyField(Subreddit)
