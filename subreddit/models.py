46e9855efde8b611c45cb452333170b96f5b530f
Marcel Cornett <marcornett@gmail.com>
2021-01-05 20:49:54 -0500

    Subreddit model that includes: title, about, created at and members. Could include rules for a subreddit but not currently implemented.

--- subreddit/models.py
+++ subreddit/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
-# Create your models here.
+from user.models import User
+class Subreddit(models.Model):
+    title = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False,
+    )
+    about = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        auto_now_add=False
+    )
+    members = models.ManyToManyField(User)
+    
