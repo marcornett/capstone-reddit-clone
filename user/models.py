4a11fedfc2a515eee1df32469a54e50180b26c86
Cory Schlaudecker <cory.schlaudecker@gmail.com>
2021-01-05 22:15:49 -0500

    fixed user model

--- user/models.py
+++ user/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from subreddit import Subreddit
+from subreddit.models import Subreddit
 
-class RedditUser(AbstractUser):
+class User(AbstractUser):
     bio = models.TextField(max_length=250)
-    likes = models.IntergerField(default=0)
-    dislikes = models.IntergerField(default=0)
+    #likes = models.IntergerField(default=0)
+    #dislikes = models.IntergerField(default=0)
     images = models.ImageField(upload_to=None)
     subreddit = models.ManyToManyField(Subreddit)
