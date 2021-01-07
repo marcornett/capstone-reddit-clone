261ee018f225f5fce399566820cd8e7ca0d76567
Marcel Cornett <marcornett@gmail.com>
2021-01-07 18:35:06 -0500

    Changes to views. Signup view gets Django usercreation form, applies info, saves, and logs the user in. Login does the same with a simple form.

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.views import View
 from authentication.forms import SignUpForm, LoginForm
+from reddituser.models import RedditUser
 
 class IndexView(View):
     def get(self, request):
@@ -16,22 +17,46 @@ class SignUpView(View):
             'form': form
         }
         return render(
-            request, 'authentication/generic_form.html', context
+            request, 'authentication/signupform.html', context
         )
     def post(self, request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            # TODO return redirect
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(
+                request, username=username, password=password)
+            if user:
+                login(request, user)
+            return redirect('index')
+        context = {'form': form}
+        return render(
+            request, 'authentication/signupform.html', context)
 
 class LoginView(View):
     def get(self, request):
         form = LoginForm()
-        context = {}
+        context = {'form': form}
         return render(
             request, 'authentication/generic_form.html', context
         )
-    # TODO Post request
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(
+                request, username=username, password=password)
+            if user:
+                login(request, user)
+            return redirect('index')
+        context = {'form': form}
+        return render(
+            request, 'authentication/index.html', context)
+
 
 class LogoutView(View):
     def get(self, request):
