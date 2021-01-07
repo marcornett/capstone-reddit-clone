25e336a4e35304f13af8a09906672e5bf29ad55c
Marcel Cornett <marcornett@gmail.com>
2021-01-07 18:37:31 -0500

    Added null and blank to bio field to allow optional field in form.

--- reddituser/models.py
+++ reddituser/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class RedditUser(AbstractUser):
-    bio = models.TextField(max_length=250)
+    bio = models.TextField(max_length=250, null=True, blank=True)
     #likes = models.IntergerField(default=0)
     #dislikes = models.IntergerField(default=0)
     # images = models.ImageField(upload_to=None)
