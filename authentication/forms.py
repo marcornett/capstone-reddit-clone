abb4929d95edbb4d64b8b39650b36999912ca13d
Marcel Cornett <marcornett@gmail.com>
2021-01-07 18:32:09 -0500

    Sign up and Login forms.

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
+from reddituser.models import RedditUser
 
 class SignUpForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = RedditUser
+        fields = UserCreationForm.Meta.fields + ('email','bio',)
 
 
 class LoginForm(forms.Form):
