3018023b52a45fb6bcfc1a3b7a7928973a913416
Marcel Cornett <marcornett@gmail.com>
2021-01-11 10:46:02 -0500

    Handle flake8 issues.

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from post import views as postViews
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
