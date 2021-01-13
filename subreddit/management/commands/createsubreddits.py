26c289fb1defdca110e0c392a5514bdc348f7ae3
Marcel Cornett <marcornett@gmail.com>
2021-01-12 19:37:52 -0500

    Create data for subreddit.

---
+++ subreddit/management/commands/createsubreddits.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from subreddit.models import Subreddit
+from mimesis import Text
+text = Text()
+
+
+class Command(BaseCommand):
+    help = 'Populates database with users'
+
+    def handle(self, *args, **options):
+        for i in range(10):
+            Subreddit.objects.create(
+                title=text.word(),
+                about=text.text(),
+            )
+        self.stdout.write(
+            self.style.SUCCESS('Successfully created 10 subreddits.')
+        )
