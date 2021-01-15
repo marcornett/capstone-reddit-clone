f3923dd211c09b849aa6dab33823997e0f40fa6a
Andrew Canter <andrew.canter.5@gmail.com>
2021-01-15 12:46:26 -0500

    can delete user profile

--- reddituser/views.py
+++ reddituser/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from reddituser.models import RedditUser
 from reddituser.forms import UpdateUserForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from post.models import Post
 
 
@@ -26,3 +28,15 @@ def user_profile_view(request, username):
         'sort_by': sort_by
         }
     return render(request, 'reddituser/user_profile.html', context)
+
+@login_required()
+def delete_profile_view(request, username):
+    if request.method == "POST":
+        user = RedditUser.objects.get(username=username)
+        if request.user == user:
+            logout(request)
+        user.delete()
+
+        return redirect('/')
+
+    return render(request, 'reddituser/delete_check.html', {'username':username})
