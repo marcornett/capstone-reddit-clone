e27634a54cf4f8eb82c42b0380173171900563db
Marcel Cornett <marcornett@gmail.com>
2021-01-15 12:02:59 -0500

    Listing user posts on user profile.

--- reddituser/views.py
+++ reddituser/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from reddituser.models import RedditUser
 from reddituser.forms import UpdateUserForm
+from post.models import Post
 
 
 def user_profile_view(request, username):
     user = RedditUser.objects.get(username=username)
     form = UpdateUserForm()
+    posts = Post.objects.filter(user=request.user)
+    sort_by = 'recent'
     if request.method == 'POST':
         form = UpdateUserForm(request.POST, request.FILES)
         if form.is_valid():
             data = form.cleaned_data
-            # breakpoint()
             if data['bio']:
                 user.bio = data['bio']
             if data['image']:
@@ -19,6 +21,8 @@ def user_profile_view(request, username):
             return redirect(f'/user/{username}')
     context = {
         'user': user,
-        'form': form
+        'form': form,
+        'posts': posts,
+        'sort_by': sort_by
         }
     return render(request, 'reddituser/user_profile.html', context)
